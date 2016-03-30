4c8f3d3b38779ae898c859d4ae77885fed0f1a16
Michael Mattson <mhmattson@gmail.com>
2016-03-29 20:26:31 -0400

    MOAR DATAS

--- Data.js
+++ Data.js
@@ -3,3 +3,18 @@ var RepublicanNationalPolling = [
   { label: 'Trump', count: 42.4 },
   { label: 'Kasich', count: 18.6 },
 ];
+
+var RepublicanNewYorkPolling = [
+  { label: 'Cruz', count: 13}
+  { label: 'Trump', count: 53}
+  { label: 'Kasich', count: 14.3}
+]
+var DemocraticNationalPolling = [
+  { label: 'Clinton', count: 50.8 }
+  { label: 'Sanders', count: 43.6}
+];
+
+var TrumpPopularity = [
+  {label: 'Unfavorable', count: 63}
+  {label: 'Favorable', count: 30}
+]
