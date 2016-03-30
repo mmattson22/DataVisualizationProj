43f24674b5edf041cb82619e3506d5acfd7c15bf
Michael Mattson <mhmattson@gmail.com>
2016-03-29 21:05:11 -0400

    bleh

--- Data.js
+++ Data.js
@@ -5,31 +5,31 @@ var RepublicanNationalPolling = [
 ];
 
 var RepublicanNewYorkPolling = [
-  { label: 'Cruz', count: 13}
-  { label: 'Trump', count: 53}
-  { label: 'Kasich', count: 14.3}
-]
+  { label: 'Cruz', count: 13},
+  { label: 'Trump', count: 53},
+  { label: 'Kasich', count: 14.3},
+];
 var DemocraticNationalPolling = [
-  { label: 'Clinton', count: 50.8 }
-  { label: 'Sanders', count: 43.6}
+  { label: 'Clinton', count: 50.8 },
+  { label: 'Sanders', count: 43.6},
 ];
 
 var TrumpPopularity = [
-  {label: 'Unfavorable', count: 63}
-  {label: 'Favorable', count: 30}
+  {label: 'Unfavorable', count: 63},
+  {label: 'Favorable', count: 30},
 ];
 
 var ClintonPopularity = [
-  {label: 'Unfavorable', count: 53.9}
-  {label: 'Favorable', count: 40.7}
+  {label: 'Unfavorable', count: 53.9},
+  {label: 'Favorable', count: 40.7},
 ];
 
 var SandersPopularity = [
-  {label: 'Unfavorable', count: 41.3}
-  {label: 'Favorable', count: 48.7}
+  {label: 'Unfavorable', count: 41.3},
+  {label: 'Favorable', count: 48.7},
 ];
 
 var DirectionOfCountry = [
-  {label: 'Right Direction', count: 27.7}
-  {label: 'Wrong Direction', count: 64.6}
+  {label: 'Right Direction', count: 27.7},
+  {label: 'Wrong Direction', count: 64.6},
 ];
